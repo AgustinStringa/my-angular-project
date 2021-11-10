154d2c9a4f5ecfb250c50fe17142095033c9046b
Agustín Stringa <stringaagustin1@gmail.com>
2021-11-09 21:47:57 -0300

    directiva *ngFor

--- src/app/entradas/entradas.component.ts
+++ src/app/entradas/entradas.component.ts
@@ -14,7 +14,7 @@ export class EntradasComponent implements OnInit {
       new Entrada('primera entrada de blog',
       'kajsdbkajsbdjaksbdjkasbd','9-11-2021','Agustin Stringa',27,
       'https://images8.alphacoders.com/522/thumb-1920-522662.jpg'),
-      new Entrada('segunda entrada de blg',
+      new Entrada('titulo',
       'un poco de lorem ipsum no vendria mal','10-11-2021','Agustin Stringa',69,
       'https://images5.alphacoders.com/302/thumb-1920-302577.jpg'),
       
