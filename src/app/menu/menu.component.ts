51689cbd724d3dd5cea3f35c8bc721a93d78eadf
Agustín Stringa <stringaagustin1@gmail.com>
2021-11-23 22:43:03 -0300

    comenzando con los estilos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,19 +9,19 @@ import {config_menu} from '../models/config_menu';
 export class MenuComponent implements OnInit {
   public title:string;
   public enlaces:Array<string>;
-  public background_color:string;
+  //public background_color:string;
   public a_color:string;
 
   constructor() { 
     this.title = config_menu.title;
     this.enlaces = config_menu.enlaces;
-    this.background_color = config_menu.background_color;
+    //this.background_color = config_menu.background_color;
     this.a_color = config_menu.a_color;
   }
 
   ngOnInit(): void {
     let menu = <HTMLElement>document.querySelector('.menu');
-    menu.style.backgroundColor = this.background_color;
+
 
   }
 
--- src/app/models/config_menu.ts
+++ src/app/models/config_menu.ts
@@ -1,6 +1,6 @@
 export var config_menu = {
     'title': 'application menu',
     'enlaces': ['inicio', 'sobre mí', 'contacto', 'github'],
-    'background_color': '#cccccc',
+    'background_color': '#46454a',
     'a_color': '#eeeeee'
 }
